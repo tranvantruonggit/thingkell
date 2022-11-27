69770c555435cd27123c4902144666543750f7eb
Tran Van Truong <truongtran@hyconinc.com>
2022-11-27 18:00:43 +0700

    This has code have some optimization that it calculate CRC for 4KB in less than 1 second.

--- Crkell.hs
+++ Crkell.hs
@@ -1,6 +1,8 @@
 module Crkell where
 import Bitkell
 
+poly = 0x4C11DB7
+bit_w = crc_cal_bit_w
 {-
    Base on the C implementation that normally use lookup table, we take that as a starting point, basically,
    the following line of code is the implementation base on each bit of the byte, so we will convert it to haskell
@@ -15,11 +17,11 @@ import Bitkell
        remainder = (remainder << 1);
    }
 -}
-crc_ll_compute remainder poly bit_w =  (remainder  <<<> 1 ) <^>  ( poly * tst_nth_bit remainder (bit_w-1) ) `msk_lsb` bit_w
+crc_ll_compute remainder =  (remainder  <<<> 1 ) <^>  ( poly * tst_nth_bit remainder (bit_w-1) ) `msk_lsb` bit_w
 
 
 -- Function to compute the polynomial bit width
-crc_cal_bit_w poly = if poly < (2^8)
+crc_cal_bit_w = if poly < (2^8)
                       then 8
                       else if poly < (2^16)
                            then 16
@@ -29,12 +31,15 @@ crc_cal_bit_w poly = if poly < (2^8)
 
 
 -- Function to calculate remainder, it is the repeated X bit of the low-level computation
-crc_cal_remainder input poly bit_w = foldl ( \i x -> crc_ll_compute i poly bit_w) (input <<<> (bit_w - 8 )) ([0..7])
+crc_cal_remainder input = foldl ( \i x -> crc_ll_compute i ) (input <<<> (bit_w - 8 )) ([0..7])
 
+crclut = map crc_cal_remainder [0..255]
+
+enhance_calc input  = crclut !! input
 
 -- CRC calculation primitive, without bit reflective setup
-crc_cal_prmtv poly initVal finalVal input = finalVal `xor_gnr` foldl (\i x -> (i <<<> 8) `xor_gnr` crc_cal_remainder (x `xor_gnr` (i <>>> (bitW - 8) )) poly bitW ) initVal input
-                                                where bitW = crc_cal_bit_w poly
+crc_cal_prmtv poly initVal finalVal input = finalVal `xor_gnr` foldl (\i x -> (i <<<> 8) `xor_gnr` enhance_calc (x `xor_gnr` (i <>>> (bitW - 8) )) ) initVal input
+                                                where bitW = crc_cal_bit_w 
                                                       xor_gnr = case bitW of
                                                             8 -> (<^>) . (<&&&>) 0xFF
                                                             16 -> (<^>) . (<&&&>) 0xFFFF
@@ -44,7 +49,7 @@ crc_cal_prmtv poly initVal finalVal input = finalVal `xor_gnr` foldl (\i x -> (i
 crc_cal poly initVal finalVal False False  = crc_cal_prmtv poly initVal finalVal 
 crc_cal poly initVal finalVal  True False   = crc_cal_prmtv poly initVal finalVal . map  (<&|&>)
 crc_cal poly initVal finalVal  False True  = reflect_gnr . crc_cal_prmtv poly initVal finalVal
-                                                where bitW = crc_cal_bit_w poly
+                                                where bitW = crc_cal_bit_w 
                                                       reflect_gnr = case bitW of
                                                          8 -> (<&|&>)
                                                          16 -> (<&&|&&>)
