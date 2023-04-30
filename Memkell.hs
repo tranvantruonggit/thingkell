a386fe45fceab0fac24e0fe52b05b17afb8988c2
Truong Tran <contact@tranvantruong.com>
2023-04-30 14:58:34 +0700

    Add more function to the memory util library

--- Memkell.hs
+++ Memkell.hs
@@ -42,7 +42,21 @@ perfectConcatSect sect1 sect2 = if getNextAddr sect1 == addr sect2
 
 -- function to concatenate 2 memsection into one
 concatSect :: MemSect -> MemSect -> Maybe MemSect
-
 concatSect sect1 sect2 = if (addr sect1) >(addr sect2)
                             then concatSect sect2 sect1
                             else perfectConcatSect (padMemUntil sect1 (addr sect2)) sect2
+--Function to take n byte from beginning and let the rest
+take :: Int -> MemSect -> Maybe MemSect
+take n sect = if (n<= getMemsecLen sect)
+                    then Just MemSect { addr = addr sect,
+                                        byteArr = Prelude.take n (byteArr sect) }
+                    else padMem sect n
+
+-- Function to remove the first n byte from memory section
+drop :: Int -> MemSect -> Maybe MemSect
+drop n sect = if (n <= getMemsecLen sect)
+                then Just MemSect { addr = n + addr sect,
+                                    byteArr = Prelude.drop n (byteArr sect) }
+                else Just MemSect { addr = n + addr sect,
+                                    byteArr = []
+                                    }
