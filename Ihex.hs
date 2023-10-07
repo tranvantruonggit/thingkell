5bcd3127ca6c17cc3c4c305c9d456d1d3eba536f
Truong Tran <contact@tranvantruong.com>
2023-10-07 21:46:25 +0700

    Apply Maybe keyword to the output consistently. Haven't seriously tested.

--- Ihex.hs
+++ Ihex.hs
@@ -2,8 +2,10 @@ module Ihex where
 import Memkell
 import Data.Maybe (catMaybes)
 import Data.Maybe (fromJust)
+import Data.Maybe (fromMaybe)
 import Control.Monad     (join)
 import Data.List.Split
+import Data.List    (groupBy)
 import Control.Monad
 import Numeric (readHex)
 import qualified Data.ByteString as B
@@ -63,11 +65,11 @@ makeDIhexRecord' (addr, dat) =
         Just r -> r
         Nothing -> error "Errror"
 
+-- | Function to make the data hex record
 makeDIhexRecord :: Int->[Word8]-> Maybe IntelHexRecord
-makeDIhexRecord addr dat = 
-    if (length dat) <=16
-        then  makeIntelHexRecord (addr <&&&> 0xFFFF) dat 0
-        else  Nothing
+makeDIhexRecord addr dat 
+    | (length dat) <=16 =  makeIntelHexRecord (addr <&&&> 0xFFFF) dat 0
+    | otherwise  = error "Error in makin data hex record"
 
 makeDIhexRecords :: Int -> [Word8] -> [IntelHexRecord]
 makeDIhexRecords addr dat = 
@@ -83,34 +85,33 @@ makeIhexRecords addr dat =
             dRecord = makeDIhexRecords addr dat
 
 -- This funcion parsing perfect memory section into the intel hex record, the provided input is the base address
-recordFromMemSect_elem:: Int -> Maybe MemSect -> Maybe IntelHexRecord
-recordFromMemSect_elem _ Nothing = Nothing
+recordFromMemSect_elem:: Int -> MemSect -> Maybe IntelHexRecord
 
-recordFromMemSect_elem base (Just sect) = if getMemsecLen (Just sect) <= 16
-                                            then makeIntelHexRecord (getStartAddr (Just sect) - base    ) (byteArr sect) 0
-                                            else error "error from recordFromMemSect_elem"
+recordFromMemSect_elem base sect 
+    |  getMemsecLen sect <= 16 = makeIntelHexRecord (getStartAddr sect - base  ) (byteArr sect) 0
+    | otherwise =  Nothing
 
 -- | Function to parse the record to the intex hex record. It basically create the series of hex record, which the first hex record is the extended record (record number 4) to hold the base addres for the whole block.
-recordFromMemSect:: Maybe MemSect -> [Maybe IntelHexRecord]
-recordFromMemSect Nothing = []
-recordFromMemSect (Just sect) = [ext]++ dataArr where
-    ext = makeIntelHexRecord_Ext  $baseAddr
-    dataArr = map (\x -> recordFromMemSect_elem baseAddr x) $ splitAlign 4 $ Just sect
-    baseAddr = getStartAddr (Just sect) <&&&> 0xFFFF0000
+recordFromMemSect:: MemSect -> Maybe [IntelHexRecord]
+recordFromMemSect sect = do
+    let baseAddr = getStartAddr sect <&&&> 0xFFFF0000
+    ext <- makeIntelHexRecord_Ext  baseAddr 
+    maybeMemSectList <- splitAlign 4 sect
+    dataArr <- mapM (\x -> recordFromMemSect_elem baseAddr x) maybeMemSectList
+    return(ext:dataArr) 
     
 -- | Function to convert the MemSect to String of hex records
-optimizedMemSect2Hex :: Maybe MemSect -> String
-optimizedMemSect2Hex Nothing = []
-optimizedMemSect2Hex (Just sect) = ext++ (concat dataArr) where
-    ext = serializeRecord.makeIntelHexRecord_Ext  $baseAddr
-    dataArr = map (\x -> serializeRecord (recordFromMemSect_elem baseAddr x)) $ splitAlign 4 $ Just sect
-    baseAddr = getStartAddr (Just sect) <&&&> 0xFFFF0000
+optimizedMemSect2Hex :: MemSect -> Maybe String
+optimizedMemSect2Hex sect = do 
+    let baseAddr = getStartAddr sect <&&&> 0xFFFF0000
+    let ext = serializeRecord =<< (makeIntelHexRecord_Ext $ baseAddr)
+    dataArr <- fmap (map (\x -> (serializeRecord =<< (recordFromMemSect_elem baseAddr x)))) ( splitAlign 4 sect )
+    fmap concat (sequence(ext:dataArr)) 
 
 
 -- This function parse the Intel HEX record to the correct format
-serializeRecord:: Maybe IntelHexRecord -> String
-serializeRecord Nothing = []
-serializeRecord (Just record) = prefix_record++checksum++"\n" where
+serializeRecord:: IntelHexRecord -> Maybe String
+serializeRecord record = Just (prefix_record++checksum++"\n") where
         prefix_record = ":"++len++address++rectype++dataArr
         len = int_2_hexstr_padding 2 $ length $ ihexData record
         rectype = int_2_hexstr_padding 2 $ ihexRecordType record
@@ -124,10 +125,17 @@ serializeRecord (Just record) = prefix_record++checksum++"\n" where
         checksum =  int_2_hexstr_padding 2 $ (0-checksum') <&&&> 0xFF
 
 -- | Function that convert the memory section in to segment of hex records, the important thing of this function is that it split the memory section into the smallel chunks of aligned memory segment upto the defined bit (in this case, it is 16 bit) because the intel hex format we are working support upto 16 bit individual addressing in the data hex record type 0.
-mem2Hex :: Maybe MemSect -> String
+mem2Hex ::  MemSect -> Maybe String
 
-mem2Hex Nothing = []
-mem2Hex (Just sect) = concat $ map optimizedMemSect2Hex $ splitAlign 16 $ Just sect
+mem2Hex sect = do
+    memSects <- splitAlign 16 sect  
+    hexString <- traverse  optimizedMemSect2Hex  memSects
+    return(concat hexString)
+
+concatMaybe :: Maybe String -> Maybe String -> Maybe String
+concatMaybe (Just s1) (Just s2) = Just (s1 ++ s2)
+concatMaybe _ _ = Nothing
+ 
 
 -- | Function to tak ethe whole Word8 array (including the unchecked checksum) and return true if the check sum are correct, otherwise return False
 verifyChecksumWord8Arr::[Word8] -> Bool
@@ -186,5 +194,13 @@ word8ToInt w = fromIntegral w
 getLinearAddress:: IntelHexRecord -> Int
 getLinearAddress record = ( ((word8ToInt (dataArray!!0)) <<<> 24) <|||>  ((word8ToInt (dataArray!!1))<<<>16) )
                         where dataArray = ihexData record
+                        
+-- | Predicate function that is used to group the array of intel hex record into segment of hex record that is led by the Extended Linear address recard
+predIhexRecords:: IntelHexRecord -> IntelHexRecord -> Bool
+predIhexRecords _ x = (ihexRecordType x) == 0
 
+-- | Function to group the array of Inhex Hex Records to array of hex records segments
+groupbySegment:: [Maybe IntelHexRecord] -> [[IntelHexRecord]]
+groupbySegment arr = groupBy predIhexRecords $ catMaybes arr
 
+-- | 
--- Main.hs
+++ Main.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE CApiFFI #-}
 import Data.Maybe (catMaybes)
+import Data.Maybe (fromMaybe)
 import Control.Exception
 import Bitkell
 import Hexkell
@@ -38,8 +39,8 @@ main = do
    let myExtRec = makeIntelHexRecord_Ext 0x90004000
    putStrLn $ show myExtRec
    print "hello"
-   let orig = Just MemSect {addr = 0x8000, byteArr =[1,2,3,4,5]}
-   let tailSect = Just MemSect {addr = 0x8008, byteArr =[]}
+   let orig =  MemSect {addr = 0x8000, byteArr =[1,2,3,4,5]}
+   let tailSect =  MemSect {addr = 0x8008, byteArr =[]}
    print $ padMem orig 8
    print $ padMemUntil orig 0x8010
    print $ MemSect 0x8010 [1]
@@ -49,12 +50,11 @@ main = do
    print $ Memkell.drop 2  ( orig)
    print $ Memkell.split 3  orig
    print $ "Check is there mem hold"
-   let x1 = Just MemSect {addr = 0x8000, byteArr = [1,2,3,4]}
-   let x2 = Just MemSect {addr = 0x8004, byteArr = [0..(4)]}
-   let x3 = Just MemSect {addr = 0x9004, byteArr = [1..65536]}
+   let x1 =  MemSect {addr = 0x8000, byteArr = [1,2,3,4]}
+   let x2 =  MemSect {addr = 0x8004, byteArr = [0..(4)]}
+   let x3 =  MemSect {addr = 0x9004, byteArr = [1..65536]}
    let x4 = MemSect {addr = 0x12347FFE , byteArr = map ( \x -> fromIntegral(x <&&&>  0xFF)) [1..(1000000)]}
    --print x4
-   print $ isPerfectMemsectPair (Nothing,Nothing)
    print $ isPerfectMemsectPair (x1,x2)
    print $ isPerfectMemSectArr [x1,x2]
    print $ isPerfectMemSectArr [x1,x2,x3]
@@ -70,9 +70,9 @@ main = do
    --print $ serializeRecord $ recordFromMemSect_elem 0 $ head ( splitAlign 4 (Just x4))
    --print $ map serializeRecord (recordFromMemSect $ (Just x4))
    print $ int_2_hexstr_padding 3 0x1234
-   let str1 = map serializeRecord $ concat $ map (recordFromMemSect) ( splitAlign 16 (Just x4))
-   let str2 =  foldl (++) [] str1
-   let str3 = mem2Hex $ Just x4
+   -- let str1 = map serializeRecord $ concat $ map (recordFromMemSect) ( splitAlign 16 ( x4))
+   -- let str2 =  foldl (++) [] str1
+   let str3 =  mem2Hex  x4
    let str5 = mem2Hex $ x1
    let str6 = mem2Hex $ x2
    print $ str5
@@ -82,7 +82,7 @@ main = do
    --putStr str2
    let file = "output.hex"
    print $ "Start writing file"
-   time $ writeFile file str3
+   time $ writeFile file  (fromMaybe "" str3)
    print $ "End writting file"
    
    print $ sort [x2, x3, x1]
@@ -91,4 +91,10 @@ main = do
    print $ hexline2record $ ":10B77000737475767778797A7B7C7D7E7F80818221"
    let list_mem_sect_1 = map hexline2record $ str_arr
    print $ list_mem_sect_1
-   print $ collectAndMerge2Memsect $ catMaybes list_mem_sect_1
+   --print $ swallow.collectAndMerge2Memsect $ catMaybes list_mem_sect_1
+   print $ test_b
+   let test_input_hex_file_content = str3
+   --let list_hex_rec = hexline2record test_input_hex_file_content
+   
+   --print $ splitOn "\n" test_input_hex_file_content
+   print $ "Hello"
