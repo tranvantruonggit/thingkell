b4981c454e5c03b010d3e515518ab82ad329f2bc
Truong Tran <contact@tranvantruong.com>
2023-12-10 15:53:39 +0700

    Sound like a good test case being formed

--- test/HexTest.hs
+++ test/HexTest.hs
@@ -1,5 +1,5 @@
 module HexTest where
-
+import MemService
 import Test.Tasty
 import Test.Tasty.HUnit
 
@@ -12,3 +12,16 @@ simpleFunctionTest = testCase "Testing a simple function" $ do
     5 @=? result
 
 -- Define the test suite
+test_ihex_parse_short_data :: TestTree
+test_ihex_parse_short_data = testCase "Test the short hex file parsing" $ do
+    -- Call the function in mem service
+    stringhex <- readHexFileLines "test/8byte_start0.hex"
+    print $ head stringhex
+
+test_iHex2HexRecards :: TestTree
+test_iHex2HexRecards = testCase "Test the function to parse the hex file into the hex records" $ do 
+    -- Call the function in mem service
+    result <- iHex2HexRecords "test/8byte_start0.hex"
+    print $ result
+    print $ "Hello"
+    
--- test/Main.hs
+++ test/Main.hs
@@ -4,11 +4,14 @@ import Test.Tasty (defaultMain, testGroup, TestTree)
 
 
 import HexTest
+main :: IO ()
 main = do
     setEnv "TASTY_TIMEOUT" "3s"
     defaultMain testSuite
 
 testSuite = testGroup "allTests"
     [
-        simpleFunctionTest
+        simpleFunctionTest,
+        test_ihex_parse_short_data,
+        test_iHex2HexRecards
     ]
